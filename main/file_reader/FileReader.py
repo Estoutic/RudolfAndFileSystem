f1041e3fea581dcad3fb61e31ca3f7f5470d24f4
Ilya udalov <ilya_udalov_04@mail.ru>
2021-12-11 00:26:01 +0300

    working version

--- main/file_reader/FileReader.py
+++ main/file_reader/FileReader.py
@@ -8,6 +8,7 @@ class FileReader:
         self.input_data = []
         self.general_dir_name = ''
         self.system_elements = {}
+        self.last_index_of_elements = 0
 
     def read_file(self):
         filepath = '/Users/estoutic/projects/RudolfAndFileSystem/resources/input_data'
@@ -21,12 +22,13 @@ class FileReader:
                 file_system_element:FileSystemElement = self.system_elements.get(self.system_elements.get(element).parent)
                 file_system_element.size += int(self.system_elements.get(element).size)
                 file_system_element.childes.append(element)
-        print(self.system_elements)
 
     def pretty_input_data(self):
         self.general_dir_name = self.input_data.pop(0)
         self.system_elements[self.general_dir_name[:-1]] = FileSystemElement(3, self.general_dir_name[:-1], None, 0)
-        for index in range(int(self.input_data.pop(0)) - 1, -1, -1):
+        last_index_of_elements = int(self.input_data.pop(0))
+        self.last_index_of_elements = last_index_of_elements
+        for index in range(last_index_of_elements - 1, -1, -1):
             split_element = self.input_data[index].split()
             if split_element[0] == '0':
                 file_system_el:FileSystemElement = FileSystemElement(split_element[0], split_element[1], split_element[2], 0)
@@ -35,5 +37,25 @@ class FileReader:
             self.system_elements[split_element[1]] = file_system_el
 
     def get_commands(self):
-        for el in self.system_elements:
-            print(f'name: {self.system_elements.get(el).name}\nsize: {self.system_elements.get(el).size}\n')
+        for index in range(self.last_index_of_elements + 1, len(self.input_data)):
+            command_info = self.input_data[index].split()
+            if command_info[0] == '0':
+                print(self.system_elements.get(command_info[1]).size)
+            elif command_info[0] == '1':
+                file_info:FileSystemElement = self.system_elements.get(command_info[1])
+                if file_info.parent is not None:
+                    parent_of_file = file_info.parent
+                    while parent_of_file is not None:
+                        self.system_elements.get(parent_of_file).size -= int(file_info.size)
+                        parent_of_file = self.system_elements.get(parent_of_file).parent
+                new_dir = command_info[2]
+                parent_of_file = file_info.parent
+                self.system_elements.get(new_dir).childes.append(command_info[1])
+                self.system_elements.get(new_dir).size += int(file_info.size)
+                while parent_of_file is not None:
+                    self.system_elements.get(parent_of_file).size += int(file_info.size)
+                    parent_of_file = self.system_elements.get(parent_of_file).parent
+
+
+        # for el in self.system_elements:
+        #     print(f'name: {self.system_elements.get(el).name}\nsize: {self.system_elements.get(el).size}\n')
